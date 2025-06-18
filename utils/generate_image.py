e93ce9e70304b0b55756cc6bbe7b187bac43bd7a
CARLOS <ctm210183@gmail.com>
2025-06-18 13:40:31 +0200

    Update generate_image.py

--- utils/generate_image.py
+++ utils/generate_image.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageResampling
+from PIL import Image, ImageDraw
 import os
 
 def generate_image(prompt):
@@ -6,6 +6,7 @@ def generate_image(prompt):
     img = Image.new("RGB", (720, 1280), color=(73, 109, 137))
     d = ImageDraw.Draw(img)
     d.text((10, 10), prompt, fill=(255, 255, 0))
-    img = img.resize((720, 1280), ImageResampling.LANCZOS)
+    img = img.resize((720, 1280), Image.LANCZOS)  # <- Esta es la forma correcta ahora
     img.save(image_path)
     return image_path
+ Fix ImageResampling usando Image.LANCZOS
