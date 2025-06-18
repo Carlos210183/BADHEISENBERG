d3db5de848e62d3efa42d9c3c3be4031564a524e
CARLOS <ctm210183@gmail.com>
2025-06-18 13:29:23 +0200

    Update generate_image.py

--- utils/generate_image.py
+++ utils/generate_image.py
@@ -1,9 +1,11 @@
+from PIL import Image, ImageDraw, ImageResampling
+import os
+
 def generate_image(prompt):
-    from PIL import Image, ImageDraw
-    import os
     image_path = "/tmp/image.jpg"
     img = Image.new("RGB", (720, 1280), color=(73, 109, 137))
     d = ImageDraw.Draw(img)
-    d.text((10,10), prompt, fill=(255,255,0))
+    d.text((10, 10), prompt, fill=(255, 255, 0))
+    img = img.resize((720, 1280), ImageResampling.LANCZOS)
     img.save(image_path)
     return image_path
